ce2ac369d8a191b7718de5c36a8fb4ebace47235
Thomas <thomas@perondas.dev>
2025-11-17 22:55:26 +0100

    Added get_file_content to pbo handle

--- src/pbo/handle.rs
+++ src/pbo/handle.rs
@@ -1,12 +1,8 @@
 use crate::pbo::{checksum::Checksum, header::BinaryHeader, mime::Mime};
 use anyhow::{ensure, Result};
 use binrw::{BinRead, NullString};
-use std::{
-    collections::HashMap,
-    io::{Seek, SeekFrom::Current},
-    path::Path,
-};
-
+use std::io::{Read, SeekFrom};
+use std::{collections::HashMap, io::Seek, path::Path};
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -57,7 +53,7 @@ impl PBOHandle {
         let blob_start = handle.stream_position()?;
         let blob_size = i64::from(files.iter().map(|f| f.size).sum::<u32>());
 
-        handle.seek(Current(blob_size + 1))?;
+        handle.seek(SeekFrom::Current(blob_size + 1))?;
 
         // Get the checksum
         let checksum = Checksum::read(&mut handle)?;
@@ -78,5 +74,30 @@ impl PBOHandle {
             blob_start,
             length,
         })
+    }
+
+    pub fn get_file_content(&mut self, filename: &str) -> Result<Vec<u8>> {
+        let file_header = self
+            .files
+            .iter()
+            .find(|f| f.filename == NullString::from(filename))
+            .ok_or_else(|| anyhow::anyhow!("File not found in PBO: {}", filename))?;
+
+        // Seek to the file's offset
+        let offset: u64 = self
+            .files
+            .iter()
+            .take_while(|f| f.filename != NullString::from(filename))
+            .map(|f| f.size as u64)
+            .sum();
+
+        self.handle
+            .seek(SeekFrom::Start(self.blob_start + offset))?;
+
+        // Read the file data
+        let mut data = vec![0; file_header.size as usize];
+        self.handle.read_exact(&mut data)?;
+
+        Ok(data)
     }
 }
